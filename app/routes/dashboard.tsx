dd73880d252fab999c0cbd741409c6de825e6cd4
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-02-23 09:27:07 +0530

    Connect to supabase db

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "react-router";
+import { createClient } from '@supabase/supabase-js'
+
+
+export const loader = async () => {
+  // Create a single supabase client for interacting with your database
+  const supabase = createClient(
+    process.env.SUPABASE_URL!, 
+    process.env.SUPABASE_ANON_TOKEN!);
+    const {data} = await supabase.from("test").select();
+    return {
+      data
+    }
+};
+
+export default function Dashboard() {
+  const data = useLoaderData();
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <pre>{JSON.stringify(data, 2, null)}</pre>
+    </>
+  )
+}
